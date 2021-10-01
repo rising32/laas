3c5f4b72b967686ed65d386c82e5834c8d8c8a14
aster-max <icatmuhammad2@gmail.com>
2021-10-01 19:23:30 +0700

    main mysql connectivity manager. params are taken from .env

---
+++ app/config/database.js
@@ -0,0 +1,14 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE
+});
+
+connection.connect(error => {
+	if (error) throw error;
+	console.log("[nodemon] database connection have been established!");
+});
+
+module.exports = connection;
