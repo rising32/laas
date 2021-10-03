63ac548a9587d50962d2b3a74c67896da271ea97
aster-max <icatmuhammad2@gmail.com>
2021-10-03 14:40:24 +0700

    populating staff and item table

--- app/model/_handbook/dbquery-dml.sql
+++ app/model/_handbook/dbquery-dml.sql
@@ -1,18 +1,30 @@
 USE laas;
 
-CREATE TABLE IF NOT EXISTS TB_GUEST (
-	username VARCHAR(15) NOT NULL PRIMARY KEY,
-	full_name VARCHAR(35) NOT NULL,
-	phone VARCHAR(13) NULL,
-	address TEXT NULL,
-	postal_code VARCHAR(6) NULL,
-	activation BOOLEAN NOT NULL,
-	created_at DATETIME NOT NULL,
-	updated_at DATETIME NOT NULL
+CREATE TABLE IF NOT EXISTS TB_ITEM (
+	item_id VARCHAR(5) NOT NULL PRIMARY KEY,
+	item_name TEXT NOT NULL,
+	item_price DECIMAL(7,2) NOT NULL
 );
 
-INSERT INTO TB_GUEST VALUES ("haspul","Haspul Naser","082125337746","Jl. Siaga Raya Komp. LAN, C13, Pejaten Barat","12520",1,NOW(),NOW());
-INSERT INTO TB_GUEST VALUES ("hanip","Hanif Salsabil Kusumaditya",NULL,NULL,NULL,1,NOW(),NOW());
+INSERT INTO TB_ITEM VALUES
+("PKN","Pakaian",8000.00),
+("SPR-S","Sprei Kecil (S)",10000.00),
+("SPR-M","Sprei Sedang (M)",15000.00),
+("SPR-L","Sprei Besar (L)",20000.00),
+("BDC-S","Berdcover Kecil (S)",20000.00),
+("BDC-M","Berdcover Sedang (M)",30000.00),
+("BDC-L","Berdcover Besar (L)",40000.00),
+("JJK","Jas/Jaket/Kebaya",15000.00),
+("HRD","Hordeng",11000.00),
+("VTR","Vitrase",12000.00),
+("MSJ","Peralatan Sholat Masjid",0.00);
+
+INSERT INTO TB_STAFF VALUES
+("admin1",CONCAT('staff_',SHA1(UNHEX(SHA1('irsyadndu1ABC')))),"Muhammad Nur Irsyad","081318420901","hotpotcookie@gmail.com","Jl. Siaga Raya Komp. LAN, C13, Pejaten Barat","12520","super",1,NOW(),NOW());
+
+INSERT INTO TB_GUEST VALUES
+("haspul","Haspul Naser","082125337746","Jl. Siaga Raya Komp. LAN, C13, Pejaten Barat","12520",1,NOW(),NOW()),
+("hanip","Hanif Salsabil Kusumaditya",NULL,NULL,NULL,1,NOW(),NOW());
 
 /* REFERENCES
 -------------
