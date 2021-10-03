98c14cc6b7eb67679ecd253b7329ad67aafe0377
aster-max <icatmuhammad2@gmail.com>
2021-10-03 14:37:09 +0700

    resolving remote address on clinet's IP

--- app/controller/guest.controller.js
+++ app/controller/guest.controller.js
@@ -4,7 +4,7 @@ exports.getallguest = (req,res) => {
 	Guest.getallguest((err,data) => {
 		if (err) { res.status(500).send({ message: err.message }); }
 		else {
-			console.log("[nodemon] retrieving all guest ...\n",data);			
+			console.log("[nodemon] retrieving all guest ..."+req.connection.remoteAddress+"\n",data);			
 			res.send(data);
 		}
 	});
@@ -14,7 +14,7 @@ exports.geteachguest = (req,res) => {
 	Guest.geteachguest(req.params.username, (err,data) => {
 		if (err) { res.status(500).send({ message: err.message }); }
 		else {
-			console.log("[nodemon] retrieving a guest by username ...\n",data);			
+			console.log("[nodemon] retrieving a guest by username ..."+req.connection.remoteAddress+"\n",data);			
 			res.send(data);
 		}
 	});
