23786581856c22aac11234674c786258ff3a2ebf
aster-max <icatmuhammad2@gmail.com>
2021-10-14 22:55:43 +0700

    related to the new function, using LIKE statement for the wildcard. loggin enable

--- app/model/guest.model.js
+++ app/model/guest.model.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const logs = require("log4js");
 const sql = require("../config/database.js");
 const Guest = (guest) => {
 	this.username = guest.username;
@@ -9,11 +11,22 @@ const Guest = (guest) => {
 	this.created_at = guest.created_at;
 	this.updated_at = guest.updated_at;
 };
-
+const Validation = require("../config/validation.js");
+// ---------------------------------------
+logs.configure({
+	appenders: { 
+  		error: { type: "file", filename: path.join(__dirname,"../../log/error.log") }, 
+  		get_request: { type: "file", filename: path.join(__dirname,"../../log/get-request.log") } },
+  	categories: { 
+  		default: { appenders: ["error"], level: "error" },
+  		get_request: { appenders: ["get_request"], level: "debug" } }
+});
+// ---------------------------------------
 Guest.getallguest = (result) => {
 	sql.query("SELECT * FROM TB_GUEST",(err,res) => {
 		if (err) {
 			console.log("[nodemon] "+err);
+			logs.getLogger("error").error(err);
 			result(null,err);
 			return;
 		}
@@ -25,9 +38,25 @@ Guest.geteachguest = (username,result) => {
 	sql.query("SELECT * FROM TB_GUEST WHERE username=?",username,(err,res) => {
 		if (err) {
 			console.log("[nodemon] "+err);
+			logs.getLogger("error").error(err);			
 			result(null,err);
 			return;
 		}
+		result(null,res);
+	});
+};
+
+Guest.getregexguest = (username,result) => {
+	var fixed_username = Validation.sanitizeWord(username,0); 
+	console.log("[nodemon] username: "+username+" >> "+fixed_username);	
+	sql.query("SELECT * FROM TB_GUEST WHERE username LIKE ?","%"+fixed_username+"%",(err,res) => {
+		if (err) {
+			console.log("[nodemon] "+err);
+			result(null,err);
+			return;
+		}
+		logs.getLogger("get_request").debug("username: "+username+" >> "+fixed_username);				
+		logs.getLogger("get_request").debug(res);		
 		result(null,res);
 	});
 };
