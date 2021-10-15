0516a09f45dab3e3121f31acde16ed687dbfc8f6
aster-max <icatmuhammad2@gmail.com>
2021-10-15 23:34:05 +0700

    fixing regex, allowing numbers

--- app/config/validation.js
+++ app/config/validation.js
@@ -1,6 +1,6 @@
 function sanitizeWord(input,idx) {
 	var in_decode = decodeURIComponent(input);
-	var in_deli = in_decode.split(/[@?^$*.+-<>{}_\\/\s/]+/);
+	var in_deli = in_decode.split(/[@?^$*.+-<>{}_\\/\s/][^1-9]+/);
 	return in_deli[idx];
 }
 
