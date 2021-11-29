54262d24079e0b14f63ca9c3102640d451faa6e2
aster-max <icatmuhammad2@gmail.com>
2021-11-29 13:49:12 +0700

    repacking projects

--- app/config/validation.js
+++
@@ -1,7 +0,0 @@
-function sanitizeWord(input,idx) {
-	var in_decode = decodeURIComponent(input);
-	var in_deli = in_decode.split(/[@?^$*.+-<>{}_\\/\s/][^1-9]+/);
-	return in_deli[idx];
-}
-
-module.exports.sanitizeWord = sanitizeWord;
