25fb04b1e38b8d9fb37f617d0a0efa344941ebdf
aster-max <icatmuhammad2@gmail.com>
2021-12-01 22:38:08 +0700

    first compiled bois. testing routes

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 import (
-	fmt 	"fmt"
-	os 		"os"
-	env 	"github.com/joho/godotenv"	
+	os		"os"
+	log		"log"
+	http	"net/http"
+	env 	"github.com/joho/godotenv"
+	mux		"github.com/gorilla/mux"	
+	_       "github.com/jinzhu/gorm/dialects/mysql"
+	route	"laas/pkg/route"
 )
 
 func main() {
 	env.Load();
-	fmt.Println(":"+os.Getenv("PORT")); }
+	r := mux.NewRouter();
+	route.GuestRoutes(r);
+	http.Handle("/",r);
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"),r));
+}
+
+
+// routes -.- controller
+// model -.- config
