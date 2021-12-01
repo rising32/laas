0c1def9f130014b57db7471f2e086d7af1a9a3f7
aster-max <icatmuhammad2@gmail.com>
2021-12-01 22:50:41 +0700

    routing reqs to controller. havent tested anything yet

---
+++ pkg/route/guest.route.go
@@ -0,0 +1,13 @@
+package route
+import (
+	laas_ctrl	"laas/pkg/controller"
+	mux			"github.com/gorilla/mux"
+)
+
+var GuestRoutes = func(router *mux.Router) {
+	router.HandleFunc("/guest/",laas_ctrl.AddGuest).Methods("POST");
+	router.HandleFunc("/guest/",laas_ctrl.GetGuest).Methods("GET");	
+	router.HandleFunc("/guest/{username}",laas_ctrl.GetGuestByUsername).Methods("GET");	
+	router.HandleFunc("/guest/{username}",laas_ctrl.UpdateGuest).Methods("PUT");	
+	router.HandleFunc("/guest/{username}",laas_ctrl.DeleteGuest).Methods("DELETE");	
+}
