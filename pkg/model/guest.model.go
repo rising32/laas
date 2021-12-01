b16e8bd81e0e3c391a4c03160c2ca48f0709c9c4
aster-max <icatmuhammad2@gmail.com>
2021-12-01 22:46:32 +0700

    templating the field on Guest table, crud function based

---
+++ pkg/model/guest.model.go
@@ -0,0 +1,51 @@
+package model
+import (
+	time	"time"	
+	gorm 	"github.com/jinzhu/gorm"
+	config	"laas/pkg/config"
+)
+
+var db *gorm.DB;
+type Guest struct {
+	gorm.Model;
+	Username	string		`gorm:""json:"username"`
+	Full_name	string		`json:"full_name"`
+	Phone		string		`json:"phone"`
+	Address		string		`json:"address"`
+	Postal_code	string		`json:"postal_code"`
+	Activation	int64 		`json:"activation"`
+	Created_at	time.Time 	`json:"created_at"`
+	Updated_at	time.Time 	`json:"updated_at"`	
+}
+
+// ubah model jadi huruf gede
+
+func init() {
+	config.InitiateDB();
+	db = config.ImportDB();
+	db.AutoMigrate(&Guest{});
+}
+
+func (g *Guest) AddGuest() *Guest {
+	db.NewRecord(g);
+	db.Create(&g);
+	return g;
+}
+
+func GetGuest() []Guest {
+	var Guests []Guest;
+	db.Find(&Guests);
+	return Guests;
+}
+
+func GetGuestByUsername(username string) (*Guest,*gorm.DB) {
+	var getGuest Guest;
+	db := db.Where("username=?",username).Find(&getGuest);
+	return &getGuest,db;
+}
+
+func DeleteGuest(username string) Guest {
+	var getGuest Guest;
+	db.Where("username=?",username).Delete(&getGuest);
+	return getGuest;
+}
