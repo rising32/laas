04dc665cfad5c24816ba10992db6bfbe848bdddf
aster-max <icatmuhammad2@gmail.com>
2021-12-02 21:56:13 +0700

    testing 3 routes for now. need more valid entry point

--- pkg/route/guest.route.go
+++ pkg/route/guest.route.go
@@ -1,15 +1,17 @@
 package route
 import (
-	fmt			"fmt"
-	laas_ctrl	"laas/pkg/controller"
-	mux			"github.com/gorilla/mux"
+	controller	"laas-go-2/pkg/controller"
+	gin			"github.com/gin-gonic/gin"
+	gorm		"github.com/jinzhu/gorm"
 )
 
-var GuestRoutes = func(router *mux.Router) {
-	router.HandleFunc("/guest/",laas_ctrl.AddGuest).Methods("POST");
-	router.HandleFunc("/guest/",laas_ctrl.GetGuest).Methods("GET");	
-	router.HandleFunc("/guest/{username}",laas_ctrl.GetGuestByUsername).Methods("GET");	
-	router.HandleFunc("/guest/{username}",laas_ctrl.UpdateGuest).Methods("PUT");	
-	router.HandleFunc("/guest/{username}",laas_ctrl.DeleteGuest).Methods("DELETE");	
-	fmt.Println("[laas] loading routes");
+func GuestRoute(db *gorm.DB) *gin.Engine {
+	r := gin.Default();
+	r.Use(func(c *gin.Context) {
+		c.Set("db",db);
+	})
+	r.GET("/guest",controller.GetGuest);
+	r.GET("/guest/:username",controller.GetGuestByUsername)	
+	r.POST("/guest",controller.AddGuest);
+	return r;
 }
