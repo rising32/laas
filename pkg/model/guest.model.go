3fb9d533c85532cd84b89ec956c147d17959d166
aster-max <icatmuhammad2@gmail.com>
2021-12-02 21:55:46 +0700

    creating da struct of current table, Guest. timestamp by default

--- pkg/model/guest.model.go
+++ pkg/model/guest.model.go
@@ -1,50 +1,17 @@
 package model
 import (
-	fmt		"fmt"
-	gorm 	"github.com/jinzhu/gorm"
-	config	"laas/pkg/config"
+	time	"time"
 )
 
-var db *gorm.DB;
 type Guest struct {
-	gorm.Model;
-	Username	string		`gorm:""json:"username"`
-	Full_name	string		`json:"full_name"`
+	Username	string		`json:"username"`
+	Fullname	string		`json:"fullname"`
 	Phone		string		`json:"phone"`
 	Address		string		`json:"address"`
-	Postal_code	string		`json:"postal_code"`
-	Activation	int64 		`json:"activation"`
-}
-
-// ubah model jadi huruf gede
-
-func init() {
-	config.InitiateDB();
-	db = config.ImportDB();
-	db.AutoMigrate(&Guest{});
-	fmt.Println("[laas] migrating Guest table");
-}
-
-func (g *Guest) AddGuest() *Guest {
-	db.NewRecord(g);
-	db.Create(&g);
-	return g;
-}
-
-func GetGuest() []Guest {
-	var Guests []Guest;
-	db.Find(&Guests);
-	return Guests;
-}
-
-func GetGuestByUsername(username string) (*Guest,*gorm.DB) {
-	var getGuest Guest;
-	db := db.Where("username=?",username).Find(&getGuest);
-	return &getGuest,db;
-}
-
-func DeleteGuest(username string) Guest {
-	var getGuest Guest;
-	db.Where("username=?",username).Delete(&getGuest);
-	return getGuest;
+	PostalCode	string		`json:"postal_code"`
+	Activation	int64		`json:"activation"`
+	Salt		string		`json:"salt"`
+	Hash		string		`json:"hash"`
+	CreatedAt	time.Time	`json:"created_at" gorm:"default:CURRENT_TIMESTAMP"`
+	UpdatedAt	time.Time	`json:"updated_at" gorm:"default:CURRENT_TIMESTAMP"`
 }
