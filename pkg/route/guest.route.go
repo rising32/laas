98747d25b2eead50e9b4c33bfb6a51e0f0176ce1
aster-max <icatmuhammad2@gmail.com>
2021-12-03 22:09:13 +0700

    adding more route: update pass,address,phone

--- pkg/route/guest.route.go
+++ pkg/route/guest.route.go
@@ -10,8 +10,11 @@ func GuestRoute(db *gorm.DB) *gin.Engine {
 	r.Use(func(c *gin.Context) {
 		c.Set("db",db);
 	})
-	r.GET("/guest",controller.GetGuest);
-	r.GET("/guest/:username",controller.GetGuestByUsername)	
-	r.POST("/guest",controller.AddGuest);
+	r.POST("/api/user/guest/add",controller.AddGuest);
+	r.GET("/api/user/guest/show",controller.GetGuest);
+	r.GET("/api/user/guest/profile/:username",controller.GetGuestByUsername);
+	r.PATCH("/api/user/guest/profile/:username/update/address",controller.UpdateGuestAddress);
+	r.PATCH("/api/user/guest/profile/:username/update/password",controller.UpdateGuestPassword);
+	r.PATCH("/api/user/guest/profile/:username/update/phone",controller.UpdateGuestPhone);	
 	return r;
 }
