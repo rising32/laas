196529aa6ad87ac22a177d29e672063488fb3e8c
aster-max <icatmuhammad2@gmail.com>
2021-12-03 22:05:44 +0700

    adding some updates func variant

--- pkg/controller/guest.controller.go
+++ pkg/controller/guest.controller.go
@@ -1,6 +1,6 @@
 package controller
 import (
-//	fmt		"fmt"
+	time	"time"
 	http	"net/http"
 	model	"laas-go-2/pkg/model"
 	util	"laas-go-2/pkg/util"	
@@ -16,6 +16,16 @@ type AddGuestBody struct {
 	PostalCode	string	`json:"postal_code"`
 	Activation	int64	`json:"activation"`
 	Password 	string	`json:"password"`
+}
+type UpdateGuestAddressBody struct {
+	Address		string	`json:"address"`
+	PostalCode	string	`json:"postal_code"`	
+}
+type UpdateGuestPasswordBody struct {
+	Password 	string	`json:"password"`	
+}
+type UpdateGuestPhoneBody struct {
+	Phone		string	`json:"phone"`	
 }
 
 func GetGuest(c *gin.Context) {
@@ -51,4 +61,57 @@ func AddGuest(c *gin.Context) {
 	db := c.MustGet("db").(*gorm.DB);
 	db.Create(&new_guest);
 	c.JSON(http.StatusOK,gin.H{"data":new_guest});	
+}
+
+func UpdateGuestAddress(c *gin.Context) {
+	var guest model.Guest;
+	db := c.MustGet("db").(*gorm.DB);
+	if err := db.Where("username = ?",c.Param("username")).First(&guest).Error; err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":"Guest is not available"});
+		return;
+	}
+	var body UpdateGuestAddressBody;
+	if err := c.ShouldBindJSON(&body); err != nil {
+	    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	    return
+	}	
+	update_guest := model.Guest{Address: body.Address,PostalCode: body.PostalCode}; //UpdatedAt: time.Now().UTC()
+	db.Model(&guest).Where("username = ?",c.Param("username")).Updates(update_guest);
+	c.JSON(http.StatusOK,gin.H{"data":guest});
+}
+
+func UpdateGuestPassword(c *gin.Context) {
+	var guest model.Guest;
+	db := c.MustGet("db").(*gorm.DB);
+	if err := db.Where("username = ?",c.Param("username")).First(&guest).Error; err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":"Guest is not available"});
+		return;
+	}
+	var body UpdateGuestPasswordBody;
+	if err := c.ShouldBindJSON(&body); err != nil {
+	    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	    return
+	}
+    get_salt := util.GenerateSalt(16);
+    get_hash := util.GenerateSaltedSHA256(string(body.Password),get_salt);	
+	update_guest := model.Guest{Salt: get_salt,Hash: get_hash};	
+	db.Model(&guest).Where("username = ?",c.Param("username")).Updates(update_guest);
+	c.JSON(http.StatusOK,gin.H{"data":guest});	
+}
+
+func UpdateGuestPhone(c *gin.Context) {
+	var guest model.Guest;
+	db := c.MustGet("db").(*gorm.DB);
+	if err := db.Where("username = ?",c.Param("username")).First(&guest).Error; err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":"Guest is not available"});
+		return;
+	}
+	var body UpdateGuestPhoneBody;
+	if err := c.ShouldBindJSON(&body); err != nil {
+	    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	    return
+	}
+	update_guest := model.Guest{Phone: body.Phone};	
+	db.Model(&guest).Where("username = ?",c.Param("username")).Updates(update_guest);
+	c.JSON(http.StatusOK,gin.H{"data":guest});	
 }
