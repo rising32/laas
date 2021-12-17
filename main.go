bfc39eb88a47e8451ca96ad20f1bf41ff3357264
aster-max <icatmuhammad2@gmail.com>
2021-12-17 22:25:16 +0700

    testing current build on mint 20.04

--- main.go
+++ main.go
@@ -1,19 +1,32 @@
 package main
 import (
 	fmt		"fmt"
+    http	"net/http"	
 	model	"laas-go-2/pkg/model"
 	route	"laas-go-2/pkg/route"
 	util	"laas-go-2/pkg/util"
-	env	    "github.com/joho/godotenv"	
+	env	    "github.com/joho/godotenv"
+	gin		"github.com/gin-gonic/gin"
 )
 
 func main() {
 	env.Load();
-	fmt.Println("check-hash:",util.GenerateSaltedSHA256("haspul1963","sJw2Wzuq6ZkJBpG0"),"\n");
+	fmt.Println("check-hash:",util.GenerateSaltedSHA256("haspul1963","k72zBr3GrXEcks64"),"\n");
 //	fmt.Println("check-hash:",util.GenerateSaltedSHA256("irsyadndu1ABC","14qJzz7ADHxtJRK4"),"\n");
 
 	db := model.IntiateDB();
 	db.AutoMigrate(&model.Guest{});
-	r  := route.GuestRoute(db);
+
+	r := gin.Default();	
+	r.LoadHTMLGlob("templates/*.html");
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK,"laas.md.html",nil)
+	});
+	route.GuestRoute(db,r);
+
+
 	r.Run()
 }
+
+// main route jadiin display tree endpoint
