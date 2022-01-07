5db4a008912216c040eb857ff31197375feceb90
aster-max <icatmuhammad2@gmail.com>
2022-01-07 21:07:33 +0700

    using update profile instead of individual

--- pkg/controller/guest.controller.go
+++ pkg/controller/guest.controller.go
@@ -55,7 +55,7 @@ func AddGuest(c *gin.Context) {
 	c.JSON(http.StatusOK,gin.H{"data":new_guest});	
 }
 
-func UpdateGuestAddress(c *gin.Context) {
+func UpdateGuestProfile(c *gin.Context) {
 	var guest model.Guest;
 	db := c.MustGet("db").(*gorm.DB);
 	if err := db.Where("username = ?",c.Param("username")).First(&guest).Error; err != nil {
@@ -67,7 +67,7 @@ func UpdateGuestAddress(c *gin.Context) {
 	    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	    return
 	}	
-	update_guest := model.Guest{Address: body.Address,PostalCode: body.PostalCode}; //UpdatedAt: time.Now().UTC()
+	update_guest := model.Guest{Fullname: body.Fullname,Phone: body.Phone,Address: body.Address,PostalCode: body.PostalCode}; //UpdatedAt: time.Now().UTC()
 	db.Model(&guest).Where("username = ?",c.Param("username")).Updates(update_guest);
 	c.JSON(http.StatusOK,gin.H{"data":guest});
 }
@@ -87,23 +87,6 @@ func UpdateGuestPassword(c *gin.Context) {
     get_salt := util.GenerateSalt(16);
     get_hash := util.GenerateSaltedSHA256(string(body.Password),get_salt);	
 	update_guest := model.Guest{Salt: get_salt,Hash: get_hash};	
-	db.Model(&guest).Where("username = ?",c.Param("username")).Updates(update_guest);
-	c.JSON(http.StatusOK,gin.H{"data":guest});	
-}
-
-func UpdateGuestPhone(c *gin.Context) {
-	var guest model.Guest;
-	db := c.MustGet("db").(*gorm.DB);
-	if err := db.Where("username = ?",c.Param("username")).First(&guest).Error; err != nil {
-		c.JSON(http.StatusBadRequest,gin.H{"error":"Guest is not available"});
-		return;
-	}
-	var body ModelGuestBody;
-	if err := c.ShouldBindJSON(&body); err != nil {
-	    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-	    return
-	}
-	update_guest := model.Guest{Phone: body.Phone};	
 	db.Model(&guest).Where("username = ?",c.Param("username")).Updates(update_guest);
 	c.JSON(http.StatusOK,gin.H{"data":guest});	
 }
@@ -132,6 +115,15 @@ func OpenGuestAccount(c *gin.Context) {
 	c.JSON(http.StatusOK,gin.H{"data":guest});		
 }
 
+func DeleteGuestAccount(c *gin.Context) {
+	var guest model.Guest;	
+	db := c.MustGet("db").(*gorm.DB);
+	if err := db.Where("username = ?",c.Param("username")).First(&guest).Error; err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":"Guest is not available"});
+		return;
+	}
+	db.Model(&guest).Where("username = ?",c.Param("username")).Delete(guest);
+	c.JSON(http.StatusOK,gin.H{"data":guest});	
+}
 
-// open & delete coming soon
 // masuk ke staff model + endpoint
