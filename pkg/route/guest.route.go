f351448423e6ffd692f5efaa01df6ef645d112ad
aster-max <icatmuhammad2@gmail.com>
2022-01-07 21:12:41 +0700

    straight out to :username no g8way

--- pkg/route/guest.route.go
+++ pkg/route/guest.route.go
@@ -11,13 +11,12 @@ func GuestRoute(db *gorm.DB, r *gin.Engine) *gin.Engine {
 	})
 	r.POST("/api/user/guest/add",controller.AddGuest);
 	r.GET("/api/user/guest/show",controller.GetGuest);
-	r.GET("/api/user/guest/profile/:username",controller.GetGuestByUsername);
-	r.PATCH("/api/user/guest/profile/:username/update/address",controller.UpdateGuestAddress);
-	r.PATCH("/api/user/guest/profile/:username/update/password",controller.UpdateGuestPassword);
-	r.PATCH("/api/user/guest/profile/:username/update/phone",controller.UpdateGuestPhone);	
-	r.PATCH("/api/user/guest/profile/:username/close",controller.CloseGuestAccount);
-	r.PATCH("/api/user/guest/profile/:username/open",controller.OpenGuestAccount);
-//	r.DELETE("/api/user/guest/profile/:username/delete",controller.DeleteGuestAccount);
+	r.GET("/api/user/guest/:username",controller.GetGuestByUsername);
+	r.PATCH("/api/user/guest/:username/update/profile",controller.UpdateGuestProfile);
+	r.PATCH("/api/user/guest/:username/update/password",controller.UpdateGuestPassword);
+	r.PATCH("/api/user/guest/:username/close",controller.CloseGuestAccount);
+	r.PATCH("/api/user/guest/:username/open",controller.OpenGuestAccount);
+	r.DELETE("/api/user/guest/:username/delete",controller.DeleteGuestAccount);
 
 	return r;
 }
